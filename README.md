9347297f37525e0d777ebfb9bfb424ea5582c72b
jonathandu07 <alphajorider@gmail.com>
2022-11-12 20:17:33 +0100

    setup

---
+++ README.md
@@ -0,0 +1,20 @@
+# php-initialisation-project
+ Repository for initializing a project in php
+
+* npm install npm
+
+If you use Node.js, you can also install Sass using npm by running
+
+--> npm install -g sass
+
+--> npm i bootstrap@5.2.2
+
+--> npm i bootstrap-icons
+
+--> npm install swiper
+
+to build the style : npm run watch:scss
+
+if you have bug :
+
+* https://www.youtube.com/watch?v=gx3l-7SsZ0A&ab_channel=DeveloperThing
---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./style/main.css">
+    <link rel="shortcut icon" href="./src/bf-plwl-base.png" type="image/x-icon">
+    <title>home</title>
+</head>
+
+<body>
+    <header>
+        <?php
+        include("./components/_disconnected-navbar.php");
+        include("./components/_connect-navbar.php");
+        ?>
+    </header>
+    <main>
+        <?php
+        include_once("");
+        ?>
+    </main>
+    <footer>
+        <?php
+        include("./components/_footer.php");
+        ?>
+    </footer>
+</body>
+
+</html>
